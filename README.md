e5167dfde5d978f2abacf4e9b71ce08784566f05
Bishrul Haq <bishhaq@gmail.com>
2023-06-15 15:36:24 +0530

    Set up basic project structure and dependencies

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# nextjs-todo-app
- This is a simple Todo application built using Next.js. It allows users to create, delete and complele tasks. The application is designed to be deployed on Google Cloud using GitHub Actions.
+# A simple Todo App with Next.js
+
+This is a simple Todo application built using Next.js. It allows users to create, delete and complele tasks. The application is designed to be deployed on Google Cloud using GitHub Actions.
+
+## Features
+
+- Create new tasks.
+- Delete tasks.
+- Mark tasks as completed.
---
+++ pages/index.js
@@ -0,0 +1,82 @@
+import { useState } from "react";
+import toDoLogo from '../public/todo.png';
+
+const TodoApp = () => {
+  const [todos, setTodos] = useState([]);
+  const [input, setInput] = useState("");
+
+  const addTodo = (e) => {
+    e.preventDefault();
+    if (!input) return;
+    setTodos([...todos, { id: Date.now(), text: input, done: false }]);
+    setInput("");
+  };
+
+  const deleteTodo = (id) => {
+    setTodos(todos.filter((todo) => todo.id !== id));
+  };
+
+  const markTodo = (id) => {
+    setTodos(
+      todos.map((todo) => (todo.id === id ? { ...todo, done: !todo.done } : todo))
+    );
+  };
+
+  return (
+    <div>
+      <section class="bg-gray-50 dark:bg-gray-900">
+        <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+          <a href="#" class="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
+            <img class="w-8 h-8 mr-2" src={toDoLogo.src} alt="logo" />
+            ToDo App
+          </a>
+          <div class="w-full p-6 bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md dark:bg-gray-800 dark:border-gray-700 sm:p-8 mb-2">
+            <form onSubmit={addTodo}>
+              <div>
+                <label for="text" class="block mb-3 text-sm font-medium text-gray-900 dark:text-white">Add your Task 😎</label>
+                <input
+                  type="text"
+                  value={input}
+                  onChange={(e) => setInput(e.target.value)}
+                  placeholder="Add a new todo"
+                  name="text"
+                  id="text"
+                  class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 focus:text-green-600"
+                  required />
+              </div>
+              <button type="submit" class="w-full mt-5 g-white bg-gray-100 hover:bg-gray-200 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow text-center">
+                Add Task
+              </button>
+            </form>
+          </div>
+
+          {todos.map((todo) => (
+            <div key={todo.id} class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md dark:bg-gray-800 dark:border-gray-700 mb-2">
+              <div class="flex flex-col md:flex-row items-center justify-center p-4">
+                <div class="md:w-3/4">
+                  <p onClick={() => markTodo(todo.id)} class={`${todo.done ? 'line-through' : ''}`}>{todo.text}</p>
+                </div>
+                <div class="md:w-1/4 flex flex-col justify-center items-center">
+                {todo.done ? (
+                  <button class="text-white font-bold py-2 px-4" disabled>
+                    Completed
+                  </button>
+                  ) : (
+                    <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={() => deleteTodo(todo.id)}>
+                      Remove
+                    </button>
+                  )}
+                </div>
+              </div>
+            </div>
+            ))}
+
+
+        </div>
+
+      </section>
+    </div>
+  );
+};
+
+export default TodoApp;
