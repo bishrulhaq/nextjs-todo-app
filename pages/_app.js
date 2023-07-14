1e028978098cd69054c03c06b27d25dba58081ee
Bishrul Haq <bishhaq@gmail.com>
2023-07-14 17:17:37 +0530

    Google Auth, Mongo DB added.

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,12 @@
 import '@/styles/globals.css'
 import { ThemeProvider } from "next-themes"
+import { SessionProvider } from 'next-auth/react'
 
 export default function App({ Component, pageProps }) {
-  return <ThemeProvider attribute="class">
-    <Component {...pageProps} />
+
+  return <SessionProvider session={pageProps.session}>
+    <ThemeProvider attribute="class">
+      <Component {...pageProps} />
     </ThemeProvider>
+  </SessionProvider>
 }
